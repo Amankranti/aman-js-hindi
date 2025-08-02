68cb8a8488bb0dc9e79469d828284c059005b7a5
Amankranti <amankranti2812@gmail.com>
2025-08-02 12:26:42 +0000

    datat Type

---
+++ 01_basics/02_dataType.js
@@ -0,0 +1,28 @@
+"use strict";// treat all JS code as newer version
+
+//alert("Hello")  // we are using node.js not browser
+
+console.log(3   
+    +
+    3) // code readibility should be high
+console.log("Aman")  
+
+let name = "Aman"
+let age= 12
+let isLoggedIn= false
+let state;  //null
+
+//number=>2 to power 53
+//nigint
+//string =>""
+//boolean=>true/false
+// null =>standalone value
+//undefined =>
+//symbol
+
+
+//object
+
+console.log(typeof("Aman"))
+console.log(typeof undefined)
+console.log(typeof null)
