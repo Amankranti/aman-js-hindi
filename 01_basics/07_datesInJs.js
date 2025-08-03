95c5e735459d5cbb65913e00bf280e56d0fb8fa9
Amankranti <amankranti2812@gmail.com>
2025-08-03 20:07:44 +0000

    array objects and dates

--- 01_basics/07_datesInJs.js
+++ 01_basics/07_datesInJs.js
@@ -1,6 +1,11 @@
 // Dates
 
 let myDate = new Date()
+// console.log(myDate);
+// console.log(myDate.toISOString());
+// console.log(myDate.toJSON());
+// console.log(myDate.toLocaleDateString())
+// console.log(myDate.toLocaleTimeString());
 // console.log(myDate.toString());
 // console.log(myDate.toDateString());
 // console.log(myDate.toLocaleString());
