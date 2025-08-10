c2010cfd07333cc5811601d9bafc950a692e8a49
Amankranti <amankranti2812@gmail.com>
2025-08-10 17:47:28 +0000

    if else

---
+++ 04_control_flow/one.js
@@ -0,0 +1,44 @@
+//if
+const temprature = 41
+
+// if(temprature ===41){
+//     console.log("less than 50");
+// } else{
+//     console.log("Temptratue is greater than 50")
+// }
+
+// <,>,<=,==,!=,===,!==
+
+//const score = 200
+// if(score>100){
+//     const power ="fly"
+//     console.log(`User power: ${power}`)
+// }
+// console.log(`User power is ${power}`);
+
+const balance= 1000
+// if(balance>500) console.log("test"),console.log("test2");
+
+// if(balance >500){
+//     console.log("less than 500");
+// } else if(balance<750) {
+//     console.log("less than 750");
+// }
+// else{
+//     console.log("less than 900");
+// }
+
+
+const isUserloggedIn =true
+const debitCard = true
+const logedInFromGoogle = true
+const logedInFromEmail = true
+
+if(isUserloggedIn&& debitCard&& 2==3){
+    console.log("Allow to buy coruse");
+}
+
+
+if(logedInFromGoogle || logedInFromEmail){
+    console.log("User logged in");
+}
